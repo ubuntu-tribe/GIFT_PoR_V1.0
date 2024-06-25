cb1ab7b337afe6037961eeebbaf0c960ead28d50
ChainSentry <132660875+Chain-Sentry@users.noreply.github.com>
2024-06-25 13:55:54 +0100

    Update minting.sol

--- src/minting.sol
+++ src/minting.sol
@@ -20,23 +20,33 @@ contract Minting {
     event ChainlinkOracleUsageToggled(bool indexed enabled);
     event TokensMinted(address indexed to, uint256 amount);
     event TokensBurned(address indexed from, uint256 amount);
+    event giftPorContractAddressUpdated(address indexed);
 
     // Constructor initializes the contract with the address of the GIFT_PoR contract.
-    constructor(address _giftPorContractAddress, address _giftTokenContractAddress) {
-        giftporContract = GIFTPoR(_giftporContractAddress);
+    constructor(
+        address _giftPorContractAddress,
+        address _giftTokenContractAddress
+    ) {
+        giftporContract = GIFTPoR(_giftPorContractAddress);
         giftTokenContract = GIFT(_giftTokenContractAddress);
     }
 
     // Allows an admin of the GIFT contract to set the address of a Chainlink oracle.
     function setChainlinkOracle(address _chainlinkOracleAddress) public {
-        require(giftporContract.hasRole(giftporContract.ADMIN_ROLE(), msg.sender), "Only admin can set Chainlink oracle");
+        require(
+            giftporContract.hasRole(giftporContract.ADMIN_ROLE(), msg.sender),
+            "Only admin can set Chainlink oracle"
+        );
         chainlinkOracle = AggregatorV3Interface(_chainlinkOracleAddress);
         emit ChainlinkOracleSet(_chainlinkOracleAddress);
     }
 
     // Allows an admin to toggle the usage of the Chainlink oracle for additional checks during minting.
     function toggleChainlinkOracleUsage() public {
-        require(giftporContract.hasRole(giftporContract.ADMIN_ROLE(), msg.sender), "Only admin can toggle Chainlink oracle usage");
+        require(
+            giftporContract.hasRole(giftporContract.ADMIN_ROLE(), msg.sender),
+            "Only admin can toggle Chainlink oracle usage"
+        );
         useChainlinkOracle = !useChainlinkOracle;
         emit ChainlinkOracleUsageToggled(useChainlinkOracle);
     }
@@ -44,30 +54,48 @@ contract Minting {
     // Function to mint new tokens. It checks if the sender is authorized and if the minting amount is within the allowed limit.
     function mint(address to, uint256 amount) public {
         require(giftporContract.isMinter(msg.sender), "Only minters can mint");
-        require(giftporContract.getMintAllowance(msg.sender) >= amount, "Minting amount exceeds allowance");
+        require(
+            giftporContract.getMintAllowance(msg.sender) >= amount,
+            "Minting amount exceeds allowance"
+        );
 
         // If using the Chainlink oracle, it verifies the allowance through Chainlink's data.
         if (useChainlinkOracle) {
-            (, int256 chainlinkAllowance,,,) = chainlinkOracle.latestRoundData();
-            require(uint256(chainlinkAllowance) >= amount, "Minting amount exceeds Chainlink allowance");
+            (, int256 chainlinkAllowance, , , ) = chainlinkOracle
+                .latestRoundData();
+            require(
+                uint256(chainlinkAllowance) >= amount,
+                "Minting amount exceeds Chainlink allowance"
+            );
         }
 
-        giftTokenContract._increaseSupply(to, amount); // Mint the tokens.
-        giftporContract.setMintAllowance(msg.sender, giftporContract.getMintAllowance(msg.sender) - amount); // Reduce the minter's allowance.
+        giftTokenContract.increaseSupply(amount); // Mint the tokens.
+        giftporContract.setMintAllowance(
+            msg.sender,
+            giftporContract.getMintAllowance(msg.sender) - amount
+        ); // Reduce the minter's allowance.
         emit TokensMinted(to, amount); // Log the minting event.
     }
 
     // Allows an admin to burn tokens from a specified account.
     function burnFrom(address account, uint256 amount) public {
-        require(giftporContract.hasRole(giftporContract.ADMIN_ROLE(), msg.sender), "Only admin can burn tokens");
+        require(
+            giftporContract.hasRole(giftporContract.ADMIN_ROLE(), msg.sender),
+            "Only admin can burn tokens"
+        );
         giftTokenContract.redeemGold(account, amount);
         emit TokensBurned(account, amount);
     }
 
     // Allows an admin to update the GIFT PoR contract
-    function updategiftporContractAddress(address _newgiftporContractAddress) public {
-        require(giftporContract.hasRole(giftporContract.ADMIN_ROLE(), msg.sender), "Only admin can update GIFT PoR contract address");
+    function updategiftporContractAddress(address _newgiftporContractAddress)
+        public
+    {
+        require(
+            giftporContract.hasRole(giftporContract.ADMIN_ROLE(), msg.sender),
+            "Only admin can update GIFT PoR contract address"
+        );
         giftporContract = GIFTPoR(_newgiftporContractAddress);
-        emit giftporContractAddressUpdated(_newgiftporContractAddress);
+        emit giftPorContractAddressUpdated(_newgiftporContractAddress);
     }
 }
